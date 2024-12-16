2674448002c7da8fefeeb6ec09d19829d6c7f2b7
Murat <murat.gaytemirov@hotmail.com>
2024-12-16 22:27:32 +0100

    made migrations

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField("Title", max_length=255)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Category author"
+    )
+
+
+class Expense(models.Model):
+    title = models.CharField("Title", max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="Category"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Expenses author"
+    )
+    total = models.PositiveIntegerField("Total expenses")
+
+
+class Income(models.Model):
+    title = models.CharField("Title", max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="Category"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Income author"
+    )
+    total = models.PositiveIntegerField("Total income")
